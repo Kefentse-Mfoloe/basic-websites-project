16c7c9bf8eb3373bfa10c861ecb0efe0aa28bef8
Kefentse-Mfoloe <kefentse.mfoloe@gmail.com>
2025-03-18 22:40:52 +0200

    Add concepts to website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import Header from "./components/header";
+import Concept from "./components/component";
+import { CORE_CONCEPTS } from "./data";
 
 function App() {
   return (
     <div>
       <Header />
+      <section id="core-concepts">
+        <ul>
+          <Concept {...CORE_CONCEPTS[0]}/>
+          <Concept {...CORE_CONCEPTS[1]}/>
+          <Concept {...CORE_CONCEPTS[2]}/>
+          <Concept {...CORE_CONCEPTS[3]}/>
+        </ul>
+      </section>
       <main>
         <h2>Time to get started!</h2>
       </main>
